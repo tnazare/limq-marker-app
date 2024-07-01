96c1475ba4f99a2efaacc053ad105abd97ea4cb0
Marie Beaulieu <mabeaulieu@coveo.com>
2024-07-01 19:02:00 -0400

    Reformat

--- features/seasonTeams/restRequestHandlers/getListOfSeasonTeamRequestHandler.ts
+++ features/seasonTeams/restRequestHandlers/getListOfSeasonTeamRequestHandler.ts
@@ -1,10 +1,10 @@
-import { prismaClient } from "@/features/common/prisma/prismaClient";
-import { restRequestHandlerBuilder } from "@/features/common/restRequestHandlers/restRequestHandlerBuilder";
-import { seasonTeamSelector } from "@/features/seasonTeams/prismaSelectors/seasonTeamSelector";
-import type { NextRequest } from "next/server";
-import type { RestRequestHandlerBuilderOptions } from "@/features/common/restRequestHandlers/restRequestHandlerBuilder";
-import { seasonTeamsListResponse } from "@/features/seasonTeams/restResponses/listOfSeasonTeamsResponse";
-import { notFoundErrorResponse } from "@/features/common/restResponses/notFoundErrorResponse";
+import {prismaClient} from "@/features/common/prisma/prismaClient";
+import {restRequestHandlerBuilder} from "@/features/common/restRequestHandlers/restRequestHandlerBuilder";
+import {seasonTeamSelector} from "@/features/seasonTeams/prismaSelectors/seasonTeamSelector";
+import type {NextRequest} from "next/server";
+import type {RestRequestHandlerBuilderOptions} from "@/features/common/restRequestHandlers/restRequestHandlerBuilder";
+import {seasonTeamsListResponse} from "@/features/seasonTeams/restResponses/listOfSeasonTeamsResponse";
+import {notFoundErrorResponse} from "@/features/common/restResponses/notFoundErrorResponse";
 import {IdParams} from "@/features/common/params/idParams";
 import {idParameterValidator} from "@/features/common/paramValidators/idParameterValidator";
 
@@ -12,8 +12,8 @@ const getListOfSeasonTeamsRequestHandlerBuilderOptions: RestRequestHandlerBuilde
   onValidateParams: idParameterValidator,
   onValidRequestAsync: async (req: NextRequest, details) => {
     if (details && details.params) {
-      const { params } = details.params;
-      const { id } = params;
+      const {params} = details.params;
+      const {id} = params;
       const seasonTeams = await prismaClient.seasonTeam.findMany({
         select: seasonTeamSelector,
         where: {
